1591dcc462639f63a500060fb4644d55d0ecded2
Beiner Fabian Perea Sanchez <losniches11@gmail.com>
2025-06-01 17:28:15 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset = "UTF-8">
+	<TItle></TItle>
+</head>
+
+	<body>
+		
+		<h1>Login</h1>
+
+		<form action="login.php" method="POST">
+
+		<label for="correo">Correo</label>
+		<input type = "email" name = "correo" placeholder = "Correo electronico" required><br><br>
+
+		<label for = "password">Contraseña</label>
+		<input type = "password" name = "password" placeholder = "Escriba su contraseña" required><br><br>
+
+		<label for = "edad">Edad</label>
+		<input type = "number" name = "edad" placeholder = "Digite su edad" min="18" max = "30" required><br><br>
+
+		<button type = "submit">Iniciar Session</html>
+
+		</form>
+
+
+</body>
+</html>
